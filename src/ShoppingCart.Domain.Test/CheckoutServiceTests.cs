42a5523387e347c770517a438a91b2feb1e2b991
Lorentz Vedeler <lrvedeler@gmail.com>
2019-12-09 23:17:16 +0100

    implement products and package deall

--- src/ShoppingCart.Domain.Test/CheckoutServiceTests.cs
+++ src/ShoppingCart.Domain.Test/CheckoutServiceTests.cs
@@ -5,16 +5,72 @@ namespace ShoppingCart.Domain.Test
     [TestClass]
     public class CheckoutServiceTests
     {
-        [TestMethod]
-        public void CanCalculatePriceForLaptop()
+        private readonly CheckoutService checkoutService;
+
+        public CheckoutServiceTests()
         {
-            var checkoutService = new CheckoutService();
+            this.checkoutService = new CheckoutService();
+        }
+        [TestMethod]
+        public void CalculatesPriceForLaptop()
+        {
             var cart = new Cart();
             cart.Add("L312", 1);
 
             var order = checkoutService.Checkout(cart);
 
             Assert.AreEqual(13999, order.SubTotal);
+        }
+
+        [TestMethod]
+        public void CalculatesShippingCosts()
+        {
+            var cart = new Cart();
+            cart.Add("LG4545", 1);
+
+            var order = checkoutService.Checkout(cart);
+
+            Assert.AreEqual(149, order.ShippingCost);
+        }
+
+        [TestMethod]
+        public void IncludesShippingCostsInTotal()
+        {
+            var cart = new Cart();
+            cart.Add("LG4545", 1);
+
+            var order = checkoutService.Checkout(cart);
+
+            Assert.AreEqual(3608, order.Total);
+        }
+
+        [TestMethod]
+        public void IncludesPackageDeal()
+        {
+            var cart = new Cart();
+            cart.Add("LG4545", 1);
+            cart.Add("HW089", 1);
+            cart.Add("LT7733", 1);
+
+            var order = checkoutService.Checkout(cart);
+
+            Assert.AreEqual(24273.45M, order.SubTotal);
+        }
+
+        [TestMethod]
+        public void DoesNotApplyDiscountToEntireOrder()
+        {
+            var cart = new Cart();
+            cart.Add("LG4545", 1);
+            cart.Add("HW089", 1);
+            cart.Add("LT7733", 1);
+            cart.Add("L312", 2);
+
+            var order = checkoutService.Checkout(cart);
+
+            Assert.AreEqual(52271.45M, order.SubTotal);
+            Assert.AreEqual(601, order.ShippingCost);
+            Assert.AreEqual(52872.45M, order.Total);
         }
     }
 }
--- src/ShoppingCart.Domain/CheckoutService.cs
+++ src/ShoppingCart.Domain/CheckoutService.cs
@@ -6,32 +6,70 @@ namespace ShoppingCart.Domain
 {
     public class CheckoutService
     {
+        const decimal SHIPPING_PRICE_PER_KG = 40;
         const string LAPTOP_SKU = "L312";
+        const decimal LAPTOP_PRICE = 13999;
+        const decimal LAPTOP_WEIGTH = 1.3M;
+
         const string SCREEN_SKU = "LG4545";
+        const decimal SCREEN_PRICE = 3459;
+        const decimal SCREEN_WEIGHT = 3.725M;
+
+        const string DESKTOP_SKU = "HW089";
+        const decimal DESKTOP_PRICE = 24399;
+        const decimal DESKTOP_WEIGHT = 7.9M;
+
+        const string WIRELESS_MOUSE_AND_KBOARD_SKU = "LT7733";
+        const decimal WIRELESS_MOUSE_AND_KBOARD_PRICE = 699;
+        const decimal WIRELESS_MOUSE_AND_KBOARD_WEIGTH = 0.8M;
 
         public Order Checkout(Cart cart)
         {
             var order = new Order();
-            bool laptopPurchased = false;
+            bool desktopBought = false;
             bool screenPurchased = false;
-            bool mousePurchased = false;
+            bool peripheralsPurchased = false;
 
-            foreach(var item in cart.Items)
+            foreach (var item in cart.Items)
             {
                 decimal price = 0;
+                decimal weight = 0;
                 if (item.Sku == LAPTOP_SKU)
                 {
-                    price = 13999;
-                    laptopPurchased = true;
+                    price = LAPTOP_PRICE;
+                    weight = LAPTOP_WEIGTH;
                 }
-                else if(item.Sku == SCREEN_SKU)
+                else if (item.Sku == SCREEN_SKU)
                 {
-                    price = 3459;
+                    price = SCREEN_PRICE;
+                    weight = SCREEN_WEIGHT;
                     screenPurchased = true;
                 }
-                
+                else if (item.Sku == DESKTOP_SKU)
+                {
+                    price = DESKTOP_PRICE;
+                    weight = DESKTOP_WEIGHT;
+                    desktopBought = true;
+                }
+                else if (item.Sku == WIRELESS_MOUSE_AND_KBOARD_SKU)
+                {
+                    price = WIRELESS_MOUSE_AND_KBOARD_PRICE;
+                    weight = WIRELESS_MOUSE_AND_KBOARD_WEIGTH;
+                    peripheralsPurchased = true;
+                }
+
                 order.SubTotal += price * item.Quantity;
+                order.ShippingCost += weight * item.Quantity * SHIPPING_PRICE_PER_KG;
                 order.Total += price * item.Quantity;
+            }
+
+            order.Total += order.ShippingCost;
+
+            if(desktopBought && screenPurchased && peripheralsPurchased) // Package deal
+            {
+                decimal discount = 0.15M * (DESKTOP_PRICE + SCREEN_PRICE + WIRELESS_MOUSE_AND_KBOARD_PRICE);
+                order.SubTotal -= discount;
+                order.Total -= discount;
             }
 
             return order;
